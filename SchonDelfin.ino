55e4fd85e81431d103d73a6542992df177dbcdfa
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2019-06-13 18:36:16 +0200

    update comment styling and content

--- SchonDelfin.ino
+++ SchonDelfin.ino
@@ -1,9 +1,9 @@
-//Dette er librariesne der skal bruges
+// Dette er bibliotekerne der skal bruges
 #include <SPI.h>
 #include <Adafruit_VS1053.h>
 #include <SD.h>
 
-//Dette er pin deffinitionen til mp3 breakoutboardet.
+// Dette er pin-definitionerne til mp3 breakoutboardet.
 #define CLK 15
 #define MISO 14
 #define MOSI 16
@@ -16,12 +16,13 @@
 Adafruit_VS1053_FilePlayer musicPlayer =
   Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
 
-//Denne variabel bruges til at bugteste koden
+// Denne variabel bruges til at debugge
 char rx;
 
-//Disse variabler refererer til de pins som kobberstrimlen læser
+// Her deklarerer og initialiserer vi de variabler vi bruger til at aflæse kobberstrimlen
 int sensorPin = 2;
 bool sensorValue = HIGH;
+// Her deklarerer og initialiserer vi en variabel vi kan bruge til at signalere status
 int controlPin = 8;
 
 void setup() {
@@ -30,7 +31,8 @@ void setup() {
   SD.begin(CARDCS);
   Serial.println("SD Card loaded");
 
-  //Dette kode starter MP3 boardet
+  // Dette kode forsøger at starte MP3 boardet og sætter derefter volumen
+  // Lykkedes det ikke at starte boardet vil koden gå ind i en uendelig while-løkke og ikke fortsætte herfra
   if (! musicPlayer.begin()) { // initialise the music player
      Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
      while (1);
@@ -38,17 +40,19 @@ void setup() {
   Serial.println("Music player begun");
   musicPlayer.setVolume(1,1);
 
-  //Dette kode initiererer pin 2 og 8 til at bruges som registrering af når man trykker på delfinen
+  // Her sætter vi pinMode
+  // Vi bruger sensorPin som et input med en indbygget pullup-modstand. Vi kan bruge den til at aflæse en værdi, der som udgangspunkt vil være HIGH og bliver LOW når sensorPin groundes
+  // Vi bruger controlPin som et output som vi kan signalere med
   pinMode(sensorPin, INPUT_PULLUP);
   pinMode(controlPin, OUTPUT);
  }
 
 void loop() {
-  //Her læser programmet værdien på pin to. Den variabel bruges så til at tjekke om der er forbindelse
+  // Her læser programmet værdien på pin to. Den variabel bruges så til at tjekke om der er forbindelse
   sensorVal = digitalRead(sensorPin);
 
-  //Her er funktionen til hvis der er forbindelse. Læg mærke til at den søger efter en sensorVal == LOW. Det er fordi det er en pin_pullup funktion, så den kigger efter det omvendte.
-  //Hvis forbindelsen ikke findes så skruer mp3 boardet lyden ned så den "slukker"
+  // Her er funktionen til hvis der er forbindelse mellem kobberstrimlen og en persen. Læg mærke til at den reagerer når sensorVal == LOW. Det er fordi vi har sat vores pinMode til INPUT_PULLUP på den pin, hvor vi har fobundet kobberstrimlen, hvilket betyder at logikken er omvendt.
+  // Hvis der ikke findes en forbindelse skruer mp3 boardet lyden ned så den "slukker"
   if (sensorVal == LOW) {
     digitalWrite(controlPin, HIGH);
     Serial.println("Playing file");
@@ -59,7 +63,7 @@ void loop() {
     musicPlayer.setVolume(0,0);
   }
 
-  //Dette funktionssegment er til at debugge koden. Den er til at man kan skrive "A" i Serial monitoren, og få mp3 boardet til at spille. Dette er for at teste om lydfilen og mp3 boardet virker.
+  // Dette funktionssegment er til at debugge koden. Den er til at man kan skrive "A" i Serielmonitoren, og få mp3 boardet til at spille. Dette er for at teste om lydfilen og mp3 boardet virker.
   if (Serial.available() > 0) {
     rx = Serial.read();
     Serial.println("Input received");
